65ffa4eb5a755a07ef652dc8cbb9060134145092
ali221 <=>
2020-12-04 12:49:25 +0500

    change backend url.....ssl applied

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
 
-  baseUrl: 'http://134.122.127.146:8000/instamunch/' // Development
+  baseUrl: 'https://development.instamunch.hypernymbiz.com/instamunch/' // Development
   // baseUrl: 'http://168.63.140.202:8001/instamunch/' // staging
   //baseUrl: 'http://68.183.27.20:8000/instamunch/' // production
 };

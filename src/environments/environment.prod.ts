52b306614d1a200e8c70cdf3264affd8c63832e8
Asad Ali <macces.tech@gmail.com>
2021-09-13 01:09:43 +0500

    base url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // baseUrl: 'http://134.122.127.146:8000/instamunch/' // Development
   // baseUrl: 'http://168.63.140.202:8001/instamunch/' // staging
-  baseUrl: 'http://137.184.26.178:8080/' // production
+  baseUrl: 'http://137.184.26.178:8080/instamunch/' // production
 };

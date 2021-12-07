2d304782e3a5ea7d71009685ce7db85a1515247a
Eng-asadali <macces.tech@gmail.com>
2021-12-07 11:11:33 +0500

    push

--- src/app/instamunch/todaypins/addtodaypins/addtodaypins.component.ts
+++ src/app/instamunch/todaypins/addtodaypins/addtodaypins.component.ts
@@ -15,6 +15,7 @@ export class AddtodaypinsComponent implements OnInit {
   images = [];
   selectedImage : any
   imageslist = []
+  video: any
   galleryForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     file: new FormControl('', [Validators.required]),
@@ -50,6 +51,7 @@ export class AddtodaypinsComponent implements OnInit {
   }
   onSubmit(value){
     console.log("submited", value)
+    value['video'] = this.video
     this.productService.addTodayPins(value).subscribe(
       result => {
         if (!result['error']) {
@@ -75,6 +77,7 @@ export class AddtodaypinsComponent implements OnInit {
   onSelectFile(event) {
     const file = event.target.files && event.target.files[0];
     this.galleryForm.value['video'] = file;
+    this.video = file
     if (file) {
       var reader = new FileReader();
       reader.readAsDataURL(file);

6b3a3ee1ae0e55f2a7179a3c55b85ea05809a53d
Eng-asadali <macces.tech@gmail.com>
2022-01-31 12:34:37 +0500

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // baseUrl: 'http://134.122.127.146:8000/instamunch/' // Development
   // baseUrl: 'http://168.63.140.202:8001/instamunch/' // staging
-  baseUrl: 'https://www.maccestech.com:90/instamunch/'// staging
+  baseUrl: 'http://www.maccestech.com:90/instamunch/'// staging
 };
